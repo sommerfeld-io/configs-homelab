8b30a0dd3bb356c27d7d1cf129233f87a340fcd3
sebastian <sebastian@sommerfeld.io>
2024-12-11 21:37:57 +0000

    feat: generate and patch config, apply tp nodes and bootstrap cluster from a bash script.

--- components/talos-pi-cluster/node-configs/README.md
+++ components/talos-pi-cluster/node-configs/README.md
@@ -2,4 +2,6 @@
 
 The config files inside this repo are auto-generated and downloaded to this location.
 
+The `*-patch.yml` files however are manually created and are used to patch the Talos config files (meaning the are an input for generating the Talos config files).
+
 See `docs/nodes/talos/installation.md` for more information on how to generate the config files.
---
+++ components/talos-pi-cluster/node-configs/talos-config.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+
+readonly CLUSTER_NAME="talos-pi-cluster"
+readonly CLUSTER_ENDPOINT="https://talos-control-pi.fritz.box:6443"
+
+readonly CONTROL_PLANE_NODE="talos-control-pi"
+readonly WORKER_NODES=(
+  "talos-worker-pi-1"
+  "talos-worker-pi-2"
+  "talos-worker-pi-3"
+)
+
+
+if [ -f "$CONTROL_PLANE_NODE.yml" ]; then
+  echo "[ERROR] $CONTROL_PLANE_NODE.yml config file already exists."
+  echo "[ERROR] Abort script to avoid overwriting existing configs."
+  echo "[ERROR] This would most likely result in a broken cluster (config)."
+  echo "[ERROR] Newly generated configs surly conflict with certs and PKI"
+  echo "[ERROR] and leave the cluster in an unmanageable state."
+  exit 8
+fi
+
+
+# Patch the config for the given node.
+#
+# @args $1: The node name
+# @args $2: The default config file (controlplane.yaml or worker.yaml) that should be patched
+function patch() {
+  echo "[INFO] Patch $2 to $1.yml"
+  talosctl machineconfig patch "$2" \
+    --patch "@$1-patch.yml" \
+    --output "$1.yml"
+}
+
+
+# Apply the config for the given node.
+#
+# @args $1: The node name
+function apply() {
+  echo "[INFO] Apply $1"
+
+  talosctl apply-config --insecure \
+    --nodes "$1.fritz.box" \
+    --file "$1.yml"
+}
+
+
+echo "[INFO] Generating Talos config --------------------------------"
+talosctl gen config "$CLUSTER_NAME" "$CLUSTER_ENDPOINT"
+
+
+echo "[INFO] Patch node configs -------------------------------------"
+patch "$CONTROL_PLANE_NODE" "controlplane.yaml"
+
+for worker in "${WORKER_NODES[@]}"; do
+  patch "$worker" "worker.yaml"
+done
+
+
+echo "[INFO] Apply node configs -------------------------------------"
+echo "[INFO] Apply $CONTROL_PLANE_NODE"
+apply "$CONTROL_PLANE_NODE"
+
+for worker in "${WORKER_NODES[@]}"; do
+  apply "$worker"
+done
+
+
+echo "[INFO] Bootstrap cluster --------------------------------------"
+talosctl bootstrap --nodes talos-control-pi.fritz.box --endpoints talos-control-pi.fritz.box
+
+
+echo "[INFO] Copy talos config into home dir ------------------------"
+cp talosconfig ~/.talos/config
+
+
+echo "[INFO] Generate kubectl config --------------------------------"
+talosctl kubeconfig
+# ?????????? cp ??????????
