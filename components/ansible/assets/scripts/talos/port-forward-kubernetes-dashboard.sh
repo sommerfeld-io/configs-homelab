b7ccdcfe0acc7fb1e03c06f8076314258e8a2a64
sebastian <sebastian@sommerfeld.io>
2025-01-19 12:01:43 +0000

    refactor: auto-determine protocol based on port

--- components/ansible/assets/scripts/talos/port-forward-kubernetes-dashboard.sh
+++ components/ansible/assets/scripts/talos/port-forward-kubernetes-dashboard.sh
@@ -4,12 +4,22 @@ readonly NAMESPACE="monitoring-logging"
 readonly PORT="8000" # 8000 = http, 8001 = https
 readonly SERVICE_ACCOUNT="kubernetes-dashboard-web"
 
+function protocol() {
+  if [ "$PORT" -eq "8001" ]; then
+    echo "https"
+  else
+    echo "http"
+  fi
+}
+
+PROTOCOL=$(protocol)
+readonly PROTOCOL
+
 echo "[INFO] Bearer Token -----------------------------"
 echo -e "${Y}"
 kubectl -n "$NAMESPACE" create token "$SERVICE_ACCOUNT"
 echo -e "${D}"
-echo
 
 echo "[INFO] Port Forward -----------------------------"
-echo -e "[INFO]   ${Y}http://$HOSTNAME.fritz.box:$PORT${D}"
+echo -e "[INFO]   ${Y}$PROTOCOL://$HOSTNAME.fritz.box:$PORT${D}"
 kubectl port-forward -n "$NAMESPACE" svc/kubernetes-dashboard-web "$PORT:$PORT" --address 0.0.0.0
