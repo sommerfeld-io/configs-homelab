8906ec8a72571e65dd4f79ca6969a86374e6e66b
sebastian <sebastian@sommerfeld.io>
2025-01-29 20:03:50 +0000

    refactor: simplify script

--- components/ansible/assets/scripts/talos/port-forward-kubernetes-dashboard.sh
+++ components/ansible/assets/scripts/talos/port-forward-kubernetes-dashboard.sh
@@ -3,20 +3,11 @@
 # Kubernetes Docs: Authentication
 # https://github.com/kubernetes/dashboard/blob/v2.0.0/docs/user/access-control/README.md#authentication
 
+readonly PORT="8000"
+readonly PROTOCOL="http"
+
 readonly NAMESPACE="monitoring-logging"
-readonly PORT="8000" # 8000 = http, 8001 = https
 readonly SERVICE_ACCOUNT="kubernetes-dashboard-web"
-
-function protocol() {
-  if [ "$PORT" -eq "8001" ]; then
-    echo "https"
-  else
-    echo "http"
-  fi
-}
-
-PROTOCOL=$(protocol)
-readonly PROTOCOL
 
 echo "[INFO] Bearer Token -----------------------------"
 echo -e "${Y}"
