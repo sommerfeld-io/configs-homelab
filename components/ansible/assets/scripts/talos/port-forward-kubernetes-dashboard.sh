80aadee6cc8649d9e3734e4fb5beb5cd41561e97
sebastian <sebastian@sommerfeld.io>
2025-02-06 21:10:12 +0000

    fix: create ClusterRole and ClusterRoleBinding from dashboard from script
    ArgoCD lacks the permissions to create ClusterRole and ClusterRoleBinding bit Kubernetes Dashboards need them to monitor more than just the default namesapce

--- components/ansible/assets/scripts/talos/port-forward-kubernetes-dashboard.sh
+++ components/ansible/assets/scripts/talos/port-forward-kubernetes-dashboard.sh
@@ -9,6 +9,20 @@ readonly PROTOCOL="https"
 
 readonly NAMESPACE="monitoring-logging"
 readonly SERVICE_ACCOUNT="kubernetes-dashboard-kong"
+readonly CLUSTER_ROLE_NAME="kubernetes-dashboard-read-only"
+
+echo "[INFO] Setup ------------------------------------"
+echo "[INFO] ClusterRole"
+kubectl create clusterrole "$CLUSTER_ROLE_NAME" \
+  --verb=get,list,watch \
+  --resource='*' \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo "[INFO] ClusterRoleBinding"
+kubectl create clusterrolebinding "$CLUSTER_ROLE_NAME" \
+  --clusterrole="$CLUSTER_ROLE_NAME" \
+  --serviceaccount="$NAMESPACE:$SERVICE_ACCOUNT" \
+  --dry-run=client -o yaml | kubectl apply -f -
 
 echo "[INFO] Bearer Token -----------------------------"
 echo -e "${Y}"
