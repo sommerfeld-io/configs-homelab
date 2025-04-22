ef3870cd3533d83f5d897135d1e48c31bb9aa385
sebastian <sebastian@sommerfeld.io>
2025-04-22 19:14:54 +0000

    test: check talos services

--- tests/inspec/talos-baseline/controls/http-services.rb
+++ tests/inspec/talos-baseline/controls/http-services.rb
@@ -6,11 +6,18 @@ control 'http-01' do
   desc 'Checks if the HTTP services are listening and are accessible'
 
   services = [
-    'http://talos-cp.fritz.box:30000', # Grafana
+    { protocol: 'http', host: 'talos-cp.fritz.box', port: '30000' }, # Grafana
   ]
 
-  services.each do |service|
-    describe http(service) do
+  services.each do |srv|
+    url = "#{srv[:protocol]}://#{srv[:host]}:#{srv[:port]}"
+
+    describe host(srv[:host], port: srv[:port], protocol: srv[:protocol]) do
+      it { should be_resolvable }
+      its('protocol') { should eq srv[:protocol] }
+    end
+
+    describe http(url) do
       its('status') { should be_in [200, 302, 307] }
       its('headers.Content-Type') { should match(/text\/html|application\/json/) }
     end
