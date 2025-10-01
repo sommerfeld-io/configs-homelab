a5cd74a5f5cdaebf8686443bb0bd2d2af7757630
sebastian <sebastian@sommerfeld.io>
2025-10-01 11:48:32 +0000

    chore: remove portainer from devcontainer

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "[INFO] Starting portainer"
-docker compose --file .devcontainer/ops/docker-compose.yml --env-file .devcontainer/ops/.env up -d
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
